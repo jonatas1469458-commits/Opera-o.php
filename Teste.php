7da2f0d54e09fcd0e713d670a0383b63d33351db
Jonataszk <jonatas1469458@gmail.com>
2026-08-14 23:55:36 -0300

    Teste.php

--- Teste.php
+++ Teste.php
@@ -1,52 +1,32 @@
 <?php
+require_once 'Operacao.php';
 
-require_once __DIR__ . '/operacao.php';
+$op1 = new Operacao();
 
-$calculadora = new Operacao();
+$op1->setValor1($_POST['valor1']);
+$op1->setValor2($_POST['valor2']);
+$operacao = $_POST['operacao'];
+$resultado = null;
 
-// Se vier via POST, usar os valores do formulário; caso contrário, valores de exemplo
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$valor1 = isset($_POST['valor1']) ? floatval($_POST['valor1']) : 0;
-	$valor2 = isset($_POST['valor2']) ? floatval($_POST['valor2']) : 0;
-	$operacao = isset($_POST['operacao']) ? $_POST['operacao'] : 'somar';
-} else {
-	$valor1 = 10;
-	$valor2 = 2;
-	$operacao = 'somar';
+if ($operacao == 'somar') {
+    $resultado = $op1->somar();
+
+} else if ($operacao == 'subtrair') {
+    $resultado = $op1->subtrair();
+
+} else if ($operacao == 'multiplicar') {
+    $resultado = $op1->multiplicar();
+
+} else if ($operacao == 'dividir') {
+    if ($op1->getValor2() == 0) {
+        $resultado = 'Erro: Divisão por zero não é permitida!';
+    } else {
+        $resultado = $op1->dividir();
+    }
+
+} else if($operacao == 'exponenciar') {
+    $resultado = $op1->exponenciar();
 }
 
-$calculadora->setValor1($valor1);
-$calculadora->setValor2($valor2);
-
-echo "Valor 1: " . $calculadora->getValor1() . "<br>";
-echo "Valor 2: " . $calculadora->getValor2() . "<br><br>";
-
-switch ($operacao) {
-	case 'somar':
-		$resultado = $calculadora->somar();
-		$label = 'Soma';
-		break;
-	case 'subtrair':
-		$resultado = $calculadora->subtrair();
-		$label = 'Subtração';
-		break;
-	case 'multiplicar':
-		$resultado = $calculadora->multiplicar();
-		$label = 'Multiplicação';
-		break;
-	case 'dividir':
-		$resultado = $calculadora->dividir();
-		$label = 'Divisão';
-		break;
-	case 'exponenciar':
-		$resultado = $calculadora->exponenciar();
-		$label = 'Exponenciação';
-		break;
-	default:
-		$resultado = 'Operação inválida';
-		$label = 'Resultado';
-}
-
-echo $label . ": " . $resultado . "<br>";
-
+echo '<h2>Resultado: ' . $resultado . '</h2>';
 ?>
